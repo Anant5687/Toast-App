f0b9eb17c6d6adb673185859d37a1b2081fe88db
Anant Jindal <anant.jindal@kredmint.com>
2025-11-09 20:27:56 +0530

    Created context for toast

--- src/context/toast.context.js
+++ src/context/toast.context.js
@@ -1,12 +1,43 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useReducer } from 'react';
 
 const ToastContext = createContext();
 
+function toastReducer(state, action) {
+  return {
+    ...state,
+    toasts: [...state.toasts, action.payload],
+  };
+}
+
 export const ToastContextProvider = ({ children }) => {
-  return <ToastContext.Provider value={{}}>{children}</ToastContext.Provider>;
+  const [state, dispatch] = useReducer(toastReducer, { toasts: [] });
+
+  const addToast = (message, type) => {
+    dispatch({ type, payload: { message, type } });
+  };
+
+  const addErrorToast = () => {
+    addToast('OOps something went wrong', 'error');
+  };
+
+  const addSuccessToast = () => {
+    addToast('Data parsed', 'success');
+  };
+
+  const addInfoToast = () => {
+    addToast('No data found', 'info');
+  };
+
+  const value = { addErrorToast, addSuccessToast, addInfoToast, state };
+
+  console.log({ state });
+
+  return (
+    <ToastContext.Provider value={value}>{children}</ToastContext.Provider>
+  );
 };
 
 export const useToastContext = () => {
-  const context = useContext(ToastContextProvider);
+  const context = useContext(ToastContext);
   return context;
 };
